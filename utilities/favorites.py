35e72d93d3d7e69010fe1cc31ee9c3f39e0a2647
josephatJ <josephatjulius24@gmail.com>
2022-02-15 12:50:45 +0300

    Add support to get favorites data dimensions details

--- utilities/favorites.py
+++ utilities/favorites.py
@@ -1,2 +1,14 @@
 
 # Josephat Mwakyusa, February 15, 2022
+
+import json
+from requests.auth import HTTPBasicAuth
+import requests
+
+async def get_favorite_details(PATH,username,password):
+    response = requests.get(PATH, auth=(username,password), verify=False)
+    if response.status_code != 200 and response.status_code != 201:
+        print(response.status_code)
+        return None
+    else:
+        return json.loads(response.content.decode('utf-8'))
